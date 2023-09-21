3481482c4b1907d93aa00f2567a09217811c8850
Victor Zapata <zapatavictor186@gmail.com>
2023-09-21 20:04:09 -0300

    pokeapi-scroll

--- Poke-Api/assets/index.js
+++ Poke-Api/assets/index.js
@@ -1,5 +1,5 @@
 const pokemonsContainer = document.querySelector("#caja")
-
+const loader = document.querySelector(".pokeballs-container");
 //Función controladora de la app. Servirá para manejar el estado de la aplicación
 
 const appState = {
@@ -44,18 +44,18 @@ const createTypeCards = (types) => {
         <span class="${tipo.type.name} poke__type">${tipo.type.name} </span>
         `
     })
-    .join("");
+        .join("");
 };
 
 //función para hacer el llamd a la API y retornar la data necesaria
-const pokemonTemplate = ( pokemon ) => {
+const pokemonTemplate = (pokemon) => {
     return {
-        id: pokemon.id, 
-        name: pokemon.name.toUpperCase(), 
-        image: pokemon.sprites.other.home.front_default, 
-        height: pokemon.height /10,
-        weight: pokemon.weight / 10, 
-        types: pokemon.types, 
+        id: pokemon.id,
+        name: pokemon.name.toUpperCase(),
+        image: pokemon.sprites.other.home.front_default,
+        height: pokemon.height / 10,
+        weight: pokemon.weight / 10,
+        types: pokemon.types,
         experience: pokemon.base_experience
     }
 };
@@ -79,6 +79,31 @@ const createPokemonCard = (pokemon) => {
     `
 };
 
+//Función para mostrar el loader y renderizar la nueva lista de pokemones, pasado un determinado tiempo
+const renderOnScroll = (pokemonList) => {
+    loader.classList.toggle("show");
+    setTimeout(() => {
+        loader.classList.toggle("show");
+        renderPokemonList ( pokemonList );
+        appState.isFetching = false;
+    }, 1500);
+};
+
+//Función auxiliar que determina si el usuario está en el bottom de la página
+const isEndOfPage = () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    const bottom = scrollTop + clientHeight >= scrollHeight - 1;
+    return bottom;
+};
+
+//Función que detecta el scroll y renderiza nuevos pokemones
+const loadNextPokemons = async () => {
+    if (isEndOfPage() && !appState.isFetching) {
+        appState.isFetching = true;
+        loadAndRenderPokemons(renderOnScroll);
+    }
+};
+
 
 //Función inicializadora
 function init() {
@@ -86,6 +111,9 @@ function init() {
         "DOMContentLoaded",
         async () => await loadAndRenderPokemons(renderPokemonList)
     );
+    window.addEventListener("scroll", async () => {
+        await loadNextPokemons();
+    });
 
 };
 
